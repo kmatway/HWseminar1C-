4cdc934fc9be5d0067fa9c7c0ab383ed5b7546b5
Ekaterina <kmatway@yandex.ru>
2022-12-14 18:12:30 +0500

    решение задачи2

---
+++ Z2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее
+
+Console.WriteLine("Введите число a = ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число b = ");
+int b = int.Parse(Console.ReadLine());
+
+if(a > b)
+{
+    Console.WriteLine("max = " + a);
+    Console.WriteLine("min = " + b);
+}
+else
+{
+    Console.WriteLine("max = " + b);
+    Console.WriteLine("min = " + a);
+}
