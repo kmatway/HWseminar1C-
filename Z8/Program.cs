e375087cd7f62f423caa8617ce1b13a1e2e90724
Ekaterina <kmatway@yandex.ru>
2022-12-15 00:00:04 +0500

    решение восьмой задачи

---
+++ Z8/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число N = ");
+int N = int.Parse(Console.ReadLine());
+
+int a = 1;
+while (a < N+1)
+{
+    if(a%2 == 0)
+    {
+        Console.WriteLine( + a );
+        a = a + 1;
+    }
+    else
+    {
+        a = a + 1;
+    }
+}
+
