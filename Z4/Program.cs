5535d84adac2d9fd1d1473c8fdb7d2b1162ea802
Ekaterina <kmatway@yandex.ru>
2022-12-14 23:30:48 +0500

    решение задачи 4

---
+++ Z4/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдает максимальное из этих чисел.
+
+Console.WriteLine("Введите число a = ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число b = ");
+int b = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число c = ");
+int c = int.Parse(Console.ReadLine());
+
+int max = a;
+if(b > max)
+{
+    max = b;
+}
+if(c > max)
+{
+    max = c;
+}
+Console.WriteLine(max);
